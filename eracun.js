7ca146c5f3bd537e0688d48ebc821111e904fd45
Luka Tavčer <lukatavcer@yahoo.com>
2016-05-08 19:36:24 +0000

    Zaključek dela na IV. nalogi

--- eracun.js
+++ eracun.js
@@ -274,8 +274,6 @@ streznik.post('/odjava', function(zahteva, odgovor) {
     odgovor.redirect('/prijava') 
 })
 
-
-
 streznik.listen(process.env.PORT, function() {
   console.log("Strežnik pognan!");
 })
